359dd755192433a0fe25c97ae7ab9f588d3bd2ac
minh-trung-nguyen <58035150+trungnguyenhuynhminh46@users.noreply.github.com>
2024-10-16 07:43:35 +0700

    PBI-3: Handle ConstraintViolationException

--- common/common-application/src/main/java/com/tuber/application/handler/GlobalExceptionHandler.java
+++ common/common-application/src/main/java/com/tuber/application/handler/GlobalExceptionHandler.java
@@ -1,8 +1,12 @@
 package com.tuber.application.handler;
 
 import com.tuber.domain.exception.DomainException;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
+import jakarta.validation.Path;
 import lombok.extern.slf4j.Slf4j;
 import com.tuber.domain.constant.ResponseCode;
+import org.hibernate.validator.internal.engine.path.PathImpl;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -11,8 +15,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Slf4j
@@ -48,6 +51,35 @@ public class GlobalExceptionHandler {
                         FieldError::getField,
                         Collectors.mapping(FieldError::getDefaultMessage, Collectors.toList())
                 ));
+    }
+
+    @ResponseBody
+    @ExceptionHandler(value = ConstraintViolationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiResponse<Object> handleConstraintViolationException(ConstraintViolationException exception) {
+        log.error(exception.getMessage(), exception);
+        return ApiResponse.builder()
+                .code(ResponseCode.VALIDATION_ERROR.getCode())
+                .messages(extractMessagesFromException(exception))
+                .build();
+    }
+
+    private Map<String, List<String>> extractMessagesFromException(ConstraintViolationException exception) {
+        return exception.getConstraintViolations().stream()
+                .reduce(new HashMap<String, List<String>>(), (map, violation) -> {
+                    String field = ((PathImpl) violation.getPropertyPath()).getLeafNode().getName();
+                    String message = violation.getMessage();
+                    map.putIfAbsent(field, Collections.emptyList());
+                    map.computeIfPresent(field, (key, value) -> {
+                        List<String> messages = new ArrayList<>(value);
+                        messages.add(message);
+                        return messages;
+                    });
+                    return map;
+                }, (map1, map2) -> {
+                    map1.putAll(map2);
+                    return map1;
+                });
     }
 
     @ResponseBody
