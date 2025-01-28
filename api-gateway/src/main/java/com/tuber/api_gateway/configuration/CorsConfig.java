81ad293f781f41e7b6d0ecb6eb92e15aaaa62aa1
minh-trung-nguyen <trungnguyenhuynhminh46@gmail.com>
2025-01-28 18:27:40 +0700

    PBI-34: Enable CORS

---
+++ api-gateway/src/main/java/com/tuber/api_gateway/configuration/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.tuber.api_gateway.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    CorsWebFilter corsWebFilter() {
+        CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.setAllowedOrigins(List.of("*"));
+        corsConfig.setAllowedHeaders(List.of("*"));
+        corsConfig.setAllowedMethods(List.of("*"));
+
+        UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfig);
+        return new CorsWebFilter(urlBasedCorsConfigurationSource);
+    }
+}
