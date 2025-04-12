fdb9ee5351d123c6820e706a83499b1e57db0037
minh-trung-nguyen <58035150+trungnguyenhuynhminh46@users.noreply.github.com>
2025-04-12 11:05:14 +0700

    PBI-74: Refactor code

--- order-service/order-messaging/src/main/java/com/tuber/order/service/messaging/mapper/OrderPaymentMessageMapper.java
+++ order-service/order-messaging/src/main/java/com/tuber/order/service/messaging/mapper/OrderPaymentMessageMapper.java
@@ -1,23 +1,32 @@
 package com.tuber.order.service.messaging.mapper;
 
-import com.tuber.kafka.order.avro.model.PaymentRequestAvroModel;
 import com.tuber.kafka.order.avro.model.PaymentOrderStatus;
+import com.tuber.kafka.order.avro.model.PaymentRequestAvroModel;
 import com.tuber.order.service.domain.outbox.model.payment.PaymentPayload;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+import org.mapstruct.Named;
 
 import java.util.UUID;
 
 @Mapper(componentModel = "spring")
-public abstract class OrderPaymentMessageMapper {
-    public PaymentRequestAvroModel orderPaymentPayloadToPaymentRequestAvroModel(PaymentPayload paymentPayload, UUID sagaId) {
-        return PaymentRequestAvroModel.newBuilder()
-                .setId(UUID.randomUUID())
-                .setSagaId(sagaId)
-                .setOrderId(paymentPayload.getOrderId())
-                .setCustomerId(paymentPayload.getCustomerId())
-                .setPrice(paymentPayload.getPrice())
-                .setCreatedAt(paymentPayload.getCreatedAt())
-                .setPaymentOrderStatus(PaymentOrderStatus.valueOf(paymentPayload.getPaymentOrderStatus()))
-                .build();
+public interface OrderPaymentMessageMapper {
+
+    @Mappings({
+            @Mapping(target = "id", expression = "java(java.util.UUID.randomUUID())"),
+            @Mapping(target = "sagaId", source = "sagaId"),
+            @Mapping(target = "orderId", source = "paymentPayload.orderId"),
+            @Mapping(target = "customerId", source = "paymentPayload.customerId"),
+            @Mapping(target = "price", source = "paymentPayload.price"),
+            @Mapping(target = "createdAt", source = "paymentPayload.createdAt"),
+            @Mapping(target = "paymentOrderStatus", source = "paymentPayload.paymentOrderStatus",
+                    qualifiedByName = "stringToPaymentOrderStatus")
+    })
+    PaymentRequestAvroModel orderPaymentPayloadToPaymentRequestAvroModel(PaymentPayload paymentPayload, UUID sagaId);
+
+    @Named("stringToPaymentOrderStatus")
+    default PaymentOrderStatus stringToPaymentOrderStatus(String status) {
+        return PaymentOrderStatus.valueOf(status);
     }
 }
