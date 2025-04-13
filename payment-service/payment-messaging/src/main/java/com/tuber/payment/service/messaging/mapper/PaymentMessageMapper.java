6dbd836d1c826d0325f36aee1cd64671b535570d
minh-trung-nguyen <58035150+trungnguyenhuynhminh46@users.noreply.github.com>
2025-04-13 15:59:46 +0700

    PBI-74: Implement PaymentMessageMapper

--- payment-service/payment-messaging/src/main/java/com/tuber/payment/service/messaging/mapper/PaymentMessageMapper.java
+++ payment-service/payment-messaging/src/main/java/com/tuber/payment/service/messaging/mapper/PaymentMessageMapper.java
@@ -5,13 +5,13 @@ import com.tuber.kafka.order.avro.model.PaymentResponseAvroModel;
 import com.tuber.payment.service.domain.dto.message.broker.PaymentRequest;
 import com.tuber.payment.service.domain.outbox.model.order.PaymentResponsePayload;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import java.util.UUID;
 
 @Mapper(componentModel = "spring")
 public abstract class PaymentMessageMapper {
-    //TODO: Implement this method
     public abstract PaymentRequest paymentRequestAvroModelToPaymentRequest(PaymentRequestAvroModel paymentRequestAvroModel);
-    //TODO: Implement this method
+    @Mapping(target = "id", expression = "java(java.util.UUID.randomUUID())")
     public abstract PaymentResponseAvroModel paymentResponsePayloadToPaymentResponseAvroModel(PaymentResponsePayload paymentResponsePayload, UUID sagaId);
 }
